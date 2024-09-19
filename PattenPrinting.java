36c9b5cd26be064ca02fe1cad183b0b5fd85c7d2
Gyanvendra Singh <gyanvendras2004@gmail.com>
2024-09-19 22:13:19 +0530

    0-1 triangle

--- PattenPrinting.java
+++ PattenPrinting.java
@@ -75,22 +75,90 @@ public class PattenPrinting{
 
 //******************************* INVERTED AND ROTATED HALF-PYRAMID **************************
 
-public class PattenPrinting{
-    public static void pyramid(int rows){
-        // OuterLoop
-        for(int i =1;i<=rows;i++){
-            // spaces
-            for(int j=1; j<=rows-i; j++){
-                System.out.print(" ");
-            }
-            // stars
-            for(int k=1; k<=i;k++){
-                System.out.print("*");
-                }
-                System.out.print("\n");
-        }
-    }
-    public static void main(String arg[]){
-            pyramid(4);
-    }
-}
+// public class PattenPrinting{
+//     public static void pyramid(int rows){
+//         // OuterLoop
+//         for(int i =1;i<=rows;i++){
+//             // spaces
+//             for(int j=1; j<=rows-i; j++){
+//                 System.out.print(" ");
+//             }
+//             // stars
+//             for(int k=1; k<=i;k++){
+//                 System.out.print("*");
+//                 }
+//                 System.out.print("\n");
+//         }
+//     }
+//     public static void main(String arg[]){
+//             pyramid(4);
+//     }
+// }
+
+
+// ***************************INVERTED HALF PYRAMIDS IN NUMBERS*********************************
+
+// public class PattenPrinting{
+   
+//     public static void pyramid(int rows){
+//         for(int i=0; i<rows; i++){
+//             for(int j=1; j<=rows-i;j++){
+//                 System.out.print(j);
+//             }
+//             System.out.print("\n");
+//         }
+//     }
+
+//     public static void main(String arg[]){
+//         pyramid(5);
+//     }
+// }
+
+
+// **************************Flod's triangle*******************
+
+// public class PattenPrinting{
+   
+//     public static void flods(int rows){
+//         int k = 1;
+//         for(int i=1; i<=rows; i++){
+//             for(int j=1; j<=i;j++){
+//                 System.out.print(k + " ");
+//                 k++;
+//             }
+//             System.out.print("\n");
+//         }
+//     }
+
+//     public static void main(String arg[]){
+//         flods(5);
+//     }
+// }
+
+
+// ********************** 0-1 triangle **********************
+
+//  public class PattenPrinting{
+   
+//     public static void ZeroOnetri(int rows){
+//         // boolean k = true;
+//         for(int i=1; i<=rows; i++){
+//             for(int j=1; j<=i;j++){
+//                 if((i+j)%2!=0){
+                    
+//                     System.out.print(0);
+//                 }
+//                 else{
+//                     // k = false;
+//                     System.out.print(1);
+//                 }
+//                 // k = !k;
+//             }
+//             System.out.print("\n");
+//         }
+//     }
+
+//     public static void main(String arg[]){
+//         ZeroOnetri(5);
+//     }
+// }
