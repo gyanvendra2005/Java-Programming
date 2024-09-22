26e00c1e3df294128c236d93b875aa9c175773a6
Gyanvendra Singh <gyanvendras2004@gmail.com>
2024-09-22 07:02:53 +0530

    SUB-ARRAYS

--- ArrayQuestions.java
+++ ArrayQuestions.java
@@ -19,26 +19,67 @@
 
 // ***************************** Reverse in array *****************************
 
-public class ArrayQuestions{
+// public class ArrayQuestions{
 
-    public static void Reverse(int arr[]){
-        int first=0;
-        int last=arr.length-1;
-        while(first<last){
-            int temp;
-            temp = arr[first];
-            arr[first] = arr[last];
-            arr[last] = temp;
-            first++;
-            last--;
-        }
-        for(int i=0; i<arr.length;i++){
-            System.out.print(arr[i]+ " ");
+//     public static void Reverse(int arr[]){
+//         int first=0;
+//         int last=arr.length-1;
+//         while(first<last){
+//             int temp;
+//             temp = arr[first];
+//             arr[first] = arr[last];
+//             arr[last] = temp;
+//             first++;
+//             last--;
+//         }
+//         for(int i=0; i<arr.length;i++){
+//             System.out.print(arr[i]+ " ");
+//         }
+//     }
+
+//     public static void main(String arg[]){
+//         int arr[] = {23,453,573,43,74,62,526};
+//            Reverse(arr);       
+//        }
+// }
+
+
+
+//  *********************** PAIRS IN ARRAY*************************
+ 
+//  public class ArrayQuestions {
+//     public static void pairsInArray(int arr[]) {
+//         for(int i=0; i<arr.length-1; i++){
+//             System.out.print("The pairs are: ");
+//             for(int j=i+1;j<arr.length;j++){
+//                 System.out.print(  "("+arr[i]+","+arr[j]+")");
+//             }
+//             System.out.print("\n");
+//         }
+//     }
+
+//     public static void main(String arg[]){
+//                 int arr[] = {2,4,6,8,10};
+//                 pairsInArray(arr);
+//     }
+//  }
+
+
+// *********************** SUB-ARRAYS********************
+
+ public class ArrayQuestions {
+    public static void pairsInArray(int arr[]) {
+        for(int i=0; i<arr.length-1; i++){
+            System.out.print("The pairs are: ");
+            for(int j=i+1;j<arr.length;j++){
+                System.out.print(  "("+arr[i]+","+arr[j]+")");
+            }
+            System.out.print("\n");
         }
     }
 
     public static void main(String arg[]){
-        int arr[] = {23,453,573,43,74,62,526};
-           Reverse(arr);       
-       }
-}
+                int arr[] = {2,4,6,8,10};
+                pairsInArray(arr);
+    }
+ }
