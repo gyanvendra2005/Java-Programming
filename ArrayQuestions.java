8c909e9fa858d25bf3fe4bd2d6666ddc20711770
Gyanvendra Singh <gyanvendras2004@gmail.com>
2024-09-25 18:54:47 +0530

    TRAPPING RAINWATER

--- ArrayQuestions.java
+++ ArrayQuestions.java
@@ -121,3 +121,41 @@
 //                 pairsInArray(arr);
 //     }
 //  }
+
+
+//  ************************ TRAPPING RAINWATER ***********************
+
+import java.util.*;
+public class ArrayQuestions{
+    public static int WaterHeight(int arr[]){
+        // max in left side
+         int n = arr.length;
+        int leftmax[] = new int[n];
+        leftmax[0]=arr[0];
+        for(int i=1; i<n;i++){
+            leftmax[i] = Math.max(leftmax[i-1] , arr[i]);
+        }
+        // max in right side
+        int rightmax[] = new int[n];
+        rightmax[n-1] = arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            rightmax[i] = Math.max(rightmax[i+1],arr[i]);
+        }
+        // min of rightmax and leftmax
+        int waterlevel;
+        int trappedwater =0;
+        for(int i=0;i<n;i++){
+            waterlevel = Math.min(leftmax[i],rightmax[i]);
+            trappedwater += waterlevel - arr[i]; 
+        }
+        // water trapped
+        // int trappedwater =0;
+        // for(int i=0)
+        return trappedwater;
+    }
+    public static void main(String args[]){
+          int arr[] = {4,2,0,6,3,2,5};
+          int ans = WaterHeight(arr);
+          System.out.print(ans);
+    }
+}
