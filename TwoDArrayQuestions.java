689c8acea4d3d3ea9859d56c2590134206139358
Gyanvendra Singh <gyanvendras2004@gmail.com>
2024-09-30 01:14:23 +0530

    searching in 2d array

--- TwoDArrayQuestions.java
+++ TwoDArrayQuestions.java
@@ -52,47 +52,104 @@
 
 //**************************DIAGONAL SUM**************************
 
+// import java.util.*;
+// public class TwoDArrayQuestionsjava{
+
+//     public static void DiagonalSum(int arr[][]){
+//         int lastColumn = 3;
+//         int mainDiagonal =0;
+//         int secondDiagonal = 0;
+//         // time complecity of this is O(n^2)
+//         // for(int i=0;i<4;i++){
+//         //     for(int j=0;j<4;j++){
+//         //         if(i==j){
+//         //             mainDiagonal += arr[i][j];
+//         //             secondDiagonal += arr[i][lastColumn];
+//         //             lastColumn--;
+//         //         }
+//         // }}
+//         // better way and its time compexcity is O(n)
+//         for(int i=0;i<4;i++){
+//             mainDiagonal+=arr[i][i];
+//             if(i!=lastColumn){
+//                 secondDiagonal+=arr[i][lastColumn];
+//             }
+//         }
+
+//         System.out.println("The sum main Diagonal is: "+mainDiagonal);
+//         System.out.println("The sum second Diagonal is: "+secondDiagonal);
+//     }
+
+//     public static void main(String arg[]){
+//         int arr[][] = new int[4][4];
+//         Scanner sc = new Scanner(System.in);
+//         for(int i =0;i<4;i++){
+//             for(int j=0;j<4;j++){
+//                System.out.print("Enter the element at row " + (i+1) + " and column " + (j+1) + " : ");
+//                arr[i][j] = sc.nextInt(); 
+//             }
+//         }
+    
+//         DiagonalSum(arr);
+
+//     }
+// }
+
+// ***************************SEARCH IN SORTED MATRIX*********************
+
 import java.util.*;
 public class TwoDArrayQuestionsjava{
 
-    public static void DiagonalSum(int arr[][]){
-        int lastColumn = 3;
-        int mainDiagonal =0;
-        int secondDiagonal = 0;
-        // time complecity of this is O(n^2)
-        // for(int i=0;i<4;i++){
-        //     for(int j=0;j<4;j++){
-        //         if(i==j){
-        //             mainDiagonal += arr[i][j];
-        //             secondDiagonal += arr[i][lastColumn];
-        //             lastColumn--;
-        //         }
-        // }}
-        // better way and its time compexcity is O(n)
-        for(int i=0;i<4;i++){
-            mainDiagonal+=arr[i][i];
-            if(i!=lastColumn){
-                secondDiagonal+=arr[i][lastColumn];
-            }
-        }
+     public static void stairSearch(int arr[][], int search){
+        // FIRST APPROACH TIME COMPLEXCITY (M+N)
+        // int row = 0;
+        // int column = 3;  
+        // while(row<arr.length && column>=0){
+        //     if(search==arr[row][column]){
+        //         System.out.print("Element found at:" +row+","+column);
+        //         break;
+        //     }
+        //     else if(search<arr[row][column]){
+        //         column--;
+        //     }
+        //     else if(search>arr[row][column]){
+        //         row++;
+        //     }
+        //     else{
+        //         System.out.print("Element not found");
+        //     }
+        // }
 
-        System.out.println("The sum main Diagonal is: "+mainDiagonal);
-        System.out.println("The sum second Diagonal is: "+secondDiagonal);
-    }
-
-    public static void main(String arg[]){
-        int arr[][] = new int[4][4];
-        Scanner sc = new Scanner(System.in);
-        for(int i =0;i<4;i++){
-            for(int j=0;j<4;j++){
-               System.out.print("Enter the element at row " + (i+1) + " and column " + (j+1) + " : ");
-               arr[i][j] = sc.nextInt(); 
+        // SECOND APPROACH TIME COMPLEXCITY (M+N)
+   
+        int row = 3;
+        int column=0;
+        while(row>=0 && column<arr.length){
+            if(search==arr[row][column]){
+                System.out.print("Element found at:" +row+","+column);
+                break;
             }
-        }
-    
-        DiagonalSum(arr);
+            else if(search<arr[row][column]){
+                column++;
+            }
+            else if(search>arr[row][column]){
+                row--;
+            }
+            else{
+                System.out.print("Element not found");
+            }
+        }}
+
+     public static void main(String arg[]) { 
+       
+        int arr[][]= {{10,20,30,40},
+                      {15,20,25,35,45},
+                      {27,29,37,48},
+                      {32,33,39,50}};
+        
+        int search = 30;
+
+        stairSearch(arr,search);
 
     }
 }
-
-
