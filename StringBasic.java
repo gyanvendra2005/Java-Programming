795a518529cf83adda4bf3c3a264d56fe5b3dfa9
Gyanvendra Singh <gyanvendras2004@gmail.com>
2024-10-01 21:25:01 +0530

    firts commit

---
+++ StringBasic.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class StringBasic {
+
+	public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        // Way to define string
+        String str = "Hello World";
+        String str2 = new String("Java Programming");
+        // Take Input of string
+        String str3;
+        str3 = sc.nextLine();
+        System.out.print(str3);
+    }
+}
