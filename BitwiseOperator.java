f3dc275b9dbbb2d7c1123818388f900890e2d0bb
Gyanvendra Singh <gyanvendras2004@gmail.com>
2024-10-05 19:32:57 +0530

    ith term in a bit number

--- BitwiseOperator.java
+++ BitwiseOperator.java
@@ -1,18 +1,39 @@
 // CHECK THE NO IS EVEN OR ODD
 
+// public class BitwiseOperator{
+//     public static void bitwiseevenodd(int num){
+//          int a =1;
+//          if((num & a) ==1){
+//             System.out.print("The entered number is odd");
+//          }
+//          else{
+//             System.out .print("The entered number is even");
+//          }
+//     }
+//     public static void main(String arg[]){
+//          int num = 56587;
+//          bitwiseevenodd(num);
+//     }
+// }
 
-public class BitwiseOperator{
-    public static void bitwiseevenodd(int num){
-         int a =1;
-         if((num & a) ==1){
-            System.out.print("The entered number is odd");
-         }
-         else{
-            System.out .print("The entered number is even");
-         }
-    }
-    public static void main(String arg[]){
-         int num = 56587;
-         bitwiseevenodd(num);
-    }
-}
+// FIND THE ith BIT OF A NUMBER
+
+// public class BitwiseOperator{
+//     public static void GetValueAti(int num,int i){
+//         int a=1; //0001
+        
+//         if((num & a<<i)==0){
+//             System.out.print("The value at "+i+" th term is:0");
+//         }
+//         else{
+//             System.out.print("The value at "+i+" th term is:1");
+//         }
+//     }
+//     public static void main(String arg[]){
+//         int num=15; // 0001111
+//         int i=3;
+//         GetValueAti(num,i);
+//     }
+// }
+
+// 
